85185ae3da7f6ee80c25196654bc9605391fd8dd
kurumicake <christy.wan.965@gmail.com>
2024-09-30 09:58:29 +0900

    express

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const { createServer } = require('node:http');
+const express = require('express');
+const app = express(); 
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('Hello World');
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
 });
